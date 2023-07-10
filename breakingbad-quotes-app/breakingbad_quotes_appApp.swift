db483efd10204ee9337dbb8529062b2028f1b17e
emildsn <emildsn00@gmail.com>
2023-07-10 15:10:09 -0300

    Initial Commit

---
+++ breakingbad-quotes-app/breakingbad_quotes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakingbad_quotes_appApp.swift
+//  breakingbad-quotes-app
+//
+//  Created by Emiliano Diaz on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct breakingbad_quotes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
