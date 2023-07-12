8507b5464019a4aa93303ce0fd8106b72713242c
emildsn <emildsn00@gmail.com>
2023-07-12 12:20:51 -0300

    add func get quotes and ui

--- breakingbad-quotes-app/ContentView.swift
+++ breakingbad-quotes-app/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var quotesData : [Quote] = []
+    let darkGreen = Color(red: 0.0, green: 0.2, blue: 0.0)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                HStack {
+                    Image("bbad").resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .offset(x: 65, y: 0)
+                    }
+                ForEach(quotesData, id: \.quote.isEmpty) { quote in
+                    Text(quote.quote)
+                        .font(.title)
+                        .foregroundColor(darkGreen)
+                        .opacity(0.9)
+                        .fontWeight(.black)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    Text(quote.author)
+                        .multilineTextAlignment(.center)
+                        .font(.subheadline)
+                        .padding()
+                    }
+            }
+            .onAppear(perform: getQuotes)
+            .padding()
         }
-        .padding()
+    }
+    
+    private func getQuotes() {
+        guard let url = URL(string: "https://api.breakingbadquotes.xyz/v1/quotes") else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            if let decodeData = try? JSONDecoder().decode([Quote].self, from: data) {
+                DispatchQueue.main.async {
+                    self.quotesData = decodeData
+                    print("Quotes count: \(self.quotesData.count)")
+                }
+            }
+        }.resume()
     }
 }
 
---
+++ breakingbad-quotes-app/Quotes.swift
@@ -0,0 +1,13 @@
+//
+//  Quotes.swift
+//  breakingbad-quotes-app
+//
+//  Created by Emiliano Diaz on 10/07/2023.
+//
+
+import Foundation
+
+struct Quote : Decodable {
+    var quote: String
+    var author: String
+}
